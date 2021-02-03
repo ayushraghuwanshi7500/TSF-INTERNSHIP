0b30501ed30b5de585864886d960243d271329ea
ayushraghuwanshi7500 <66081125+ayushraghuwanshi7500@users.noreply.github.com>
2021-02-03 15:13:21 +0530

    create user (frontend)

--- resources/views/createuser.blade.php
+++ resources/views/createuser.blade.php
@@ -1,12 +1,72 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-<title>Create User</title>
+    <title>CREATE USER</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous" />
+    <style>
+
+    </style>
 </head>
+
 <body>
 
-<h1>Create User</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <h2>T.S.F. BANK</h2>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home </a>
+                </li>
+                <li class="nav-item  active">
+                    <a class="nav-link" href="/createUser">Create a User<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/makeTransaction">Make a Transaction</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/transactionHistory">Transaction History</a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+    <br>
+    <div class="container">
+        <div class="jumbotron">
+            <h3  style="text-align: center;">Create User</h3>
+            <br>
+            <form action="">
+                <div class="form-group row">
+                    <label class="col-lg-4 text-center" for="name" > <h6>Name</h6> </label>
+                    <input  type="text" placeholder="Enter the name of the user" class="form-control col-lg-6 text-left" id="name" aria-describedby="name" required>
+                </div>
+                <div class="form-group row">
+                    <label class="col-lg-4 text-center" for="emailid" > <h6>Email address</h6> </label>
+                    <input  type="email" placeholder="Enter the email address of the user" class="form-control col-lg-6 text-left" id="emailid" aria-describedby="emailHelp" required>
+                </div>
+                <div class="form-group row">
+                    <label class="col-lg-4 text-center" for="balance" > <h6>Balance in Account</h6> </label>
+                    <input  type="number" placeholder="Enter the balance in the account of the user" class="form-control col-lg-6 text-left" id="balance" aria-describedby="balance" required>
+                </div>
+                <br>
+                <div class="form-group row">
+                    <div class="col col-lg-12 text-center">
+                        <button type="button" class="btn btn-success ">Create User</button>
+                        <button type="button" class="btn btn-danger ">Reset Info</button>
+                    </div>
+                </div>
+                
+            </form>
+        </div>
+    </div>
 
 
 </body>
-</html>
--- resources/views/makeatransaction.blade.php
+++ resources/views/makeatransaction.blade.php
@@ -1,12 +1,41 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-<title>Make a Transaction</title>
+    <title>MAKE A TRANSACTION</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous" />
+    <style>
+    
+    </style>
 </head>
+
 <body>
 
-<h1>Make a Transaction</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <h2>T.S.F. BANK</h2>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="/createUser">Create a User</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="/makeTransaction">Make a Transaction<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/transactionHistory">Transaction History</a>
+                </li>
+            </ul>
 
+        </div>
+    </nav>
 </body>
-</html>
